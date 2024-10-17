7455876855435208a6f018521c09df6b03b85d77
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-17 11:42:00 +0800

    Create main.py
    This python program detects faces through the camera of a laptop or PC. which is when there is a face. the program will say. face detected. when the face is not there. face not detected

---
+++ main.py
@@ -0,0 +1,36 @@
+before using this code. you have to install the library first
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5)
+
+    if len(faces) > 0:
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.putText(frame, "Face Detected", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    else:
+        # Jika tidak ada wajah yang terdeteksi, tampilkan teks merah
+        cv2.putText(frame, "Face NOT Detected", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    
+    cv2.imshow("Video", frame)
+
+    # Tekan 'q' untuk keluar
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+      
+cap.release()
+cv2.destroyAllWindows()
